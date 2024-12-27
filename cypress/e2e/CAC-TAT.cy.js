2ec877c86abe7837d7bafc1d9830d5c55f621f6d
Caroline Ferraz <projetos.crrz@gmail.com>
2024-12-27 19:26:12 -0300

    Feat Aula 9: Digitando em campos e clicando em elementos - exercicio

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,8 +1,18 @@
 // <reference types="cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-  it('verifica o título da aplicação', function() {
-    cy.visit('./src/index.html')
+    beforeEach(function() {
+      cy.visit('./src/index.html')
+    })
+    it('verifica o título da aplicação', function() {   
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  })
+    }) 
+    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+      cy.get('#firstName').type('Fulano')
+      cy.get('#lastName').type('de Tal')
+      cy.get('#email').type('fulano@detal.com')
+      cy.get('#open-text-area').type('Mensagem de teste')
+      cy.get('button[type="submit"]').click()
+      cy.get('.success').should('be.visible')
+    })
 })
