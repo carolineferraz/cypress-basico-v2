008755b1aab9b88b6b8cf7aed976038231ba7b02
Caroline Ferraz <projetos.crrz@gmail.com>
2025-01-04 11:50:07 -0300

    Feat Aula 54: avançando no uso do Cypress - cy.request() - exercicio extra 4

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -214,4 +214,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       .invoke('val', LONG_TEXT)
       .should('have.value', LONG_TEXT)
   })
+
+  it('faz uma requisição HTTP', function () {
+    cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+      .should((response) => {
+        const { status, statusText, body } = response // desestruturação do objeto response
+        expect(status).to.equal(200)
+        expect(statusText).to.equal('OK')
+        expect(body).to.include('CAC TAT')
+      })
+  })
 })
